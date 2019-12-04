e99619c2f466d4600136ba385ec31c320927874b
Bailey Pleva <pleva.bailey@gmail.com>
2019-12-03 18:01:05 -0600

    Remove letter density plot and replace it with a table. Add shinydashboard package and reformat dashboard to look generally nicer. Put the rsconnect (for shinyapps.io) directory in the .gitignore file.

--- app.R
+++ app.R
@@ -1,7 +1,8 @@
 library(shiny)
-library(shinythemes)
+library(shinydashboard)
 library(tidyverse)
 library(plotly)
+library(DT)
 
 # Load data ----
 
@@ -12,6 +13,7 @@ df <-
   # "IDOCdoc_yes_no" does not contain any information
   select(-c("IDOCdoc_yes_no"))
 
+# Create variables ----
 # Create function to create vectors from df columns in alphabetical order
 # with unique values for dashboard filter dropdowns.
 alpha_order_unique <- function(vector) {
@@ -59,137 +61,152 @@ v.demo <- c(
 
 `%notin%` <- Negate(`%in%`)
 
+
 # application UI setup ----
-ui <- fluidPage(
-  
-  theme = shinytheme("lumen"),
-  
-  titlePanel("Prisoner Response Letter Dashboard"),
-  
-  fluidRow(
-    column(
-      width = 3,
-      verticalLayout(
-        dateRangeInput(
-          inputId = "dob",
-          label = "Date of Birth Range",
-          start = "1930-01-01",
-          format = "mm-dd-yyyy"
-        ),
-        dateRangeInput(
-          inputId = "admin_date",
-          label = "Admittance Date",
-          start = "1930-01-01",
-          format = "mm-dd-yyyy"
-        ),
-        dateRangeInput(
-          inputId = "release",
-          label = "Projected Release Date",
-          start = "2000-01-01",
-          format = "mm-dd-yyyy"
-        ),
-        dateRangeInput(
-          inputId = "received",
-          label = "Letter Received",
-          start = "2010-01-01",
-          format = "mm-dd-yyyy"
-        ),
-        dateRangeInput(
-          inputId = "processed",
-          label = "Letter Processed",
-          start = "2010-01-01",
-          format = "mm-dd-yyyy"
-        ),
-        selectInput(
-          inputId = "mailed_from",
-          label = "Facility Mailed From",
-          choices = v.mailed_from,
-          multiple = TRUE
-        ),
-        selectInput(
-          inputId = "about",
-          label = "Facility About",
-          choices = v.about,
-          multiple = TRUE
-        ),
-        selectInput(
-          inputId = "code",
-          label = "Code",
-          choices = v.code,
-          multiple = TRUE
-        ),
-        selectInput(
-          inputId = "subcode",
-          label = "Sub-code",
-          choices = v.subcode,
-          multiple = TRUE
-        ),
-        selectInput(
-          inputId = "offense",
-          label = "Offense Type",
-          choices = v.offense,
-          multiple = TRUE
-        ),
-        selectInput(
-          inputId = "race",
-          label = "Race",
-          choices = v.race,
-          multiple = TRUE
-        ),
-        selectInput(
-          inputId = "gender",
-          label = "Gender",
-          choices = v.gender,
-          multiple = TRUE
+ui <- dashboardPage(
+  dashboardHeader(),
+  dashboardSidebar(
+    titlePanel(
+      "Filters"
+    ),
+    dateRangeInput(
+      inputId = "dob",
+      label = "Date of Birth Range",
+      start = "1930-01-01",
+      format = "mm-dd-yyyy"
+    ),
+    dateRangeInput(
+      inputId = "admin_date",
+      label = "Admittance Date",
+      start = "1930-01-01",
+      format = "mm-dd-yyyy"
+    ),
+    dateRangeInput(
+      inputId = "release",
+      label = "Projected Release Date",
+      start = "2000-01-01",
+      format = "mm-dd-yyyy"
+    ),
+    dateRangeInput(
+      inputId = "received",
+      label = "Letter Received",
+      start = "2010-01-01",
+      format = "mm-dd-yyyy"
+    ),
+    dateRangeInput(
+      inputId = "processed",
+      label = "Letter Processed",
+      start = "2010-01-01",
+      format = "mm-dd-yyyy"
+    ),
+    selectInput(
+      inputId = "mailed_from",
+      label = "Facility Mailed From",
+      choices = v.mailed_from,
+      multiple = TRUE
+    ),
+    selectInput(
+      inputId = "about",
+      label = "Facility About",
+      choices = v.about,
+      multiple = TRUE
+    ),
+    selectInput(
+      inputId = "code",
+      label = "Code",
+      choices = v.code,
+      multiple = TRUE
+    ),
+    selectInput(
+      inputId = "subcode",
+      label = "Sub-code",
+      choices = v.subcode,
+      multiple = TRUE
+    ),
+    selectInput(
+      inputId = "offense",
+      label = "Offense Type",
+      choices = v.offense,
+      multiple = TRUE
+    ),
+    selectInput(
+      inputId = "race",
+      label = "Race",
+      choices = v.race,
+      multiple = TRUE
+    ),
+    selectInput(
+      inputId = "gender",
+      label = "Gender",
+      choices = v.gender,
+      multiple = TRUE
+    )
+  ),
+  dashboardBody(
+    titlePanel("Prisoner Response Letter Dashboard"),
+    fluidRow(
+      column(
+        width = 6,
+        # plotlyOutput("letterDensityPlot")
+        DTOutput("letterTable")
+      ),
+      column(
+        width = 6,
+        verticalLayout(
+          selectInput(
+            inputId = "timeDimension",
+            label = "Time Dimension",
+            choices = v.time,
+            multiple = FALSE
+          ),
+          plotlyOutput("timeDensityPlot")
         )
       )
     ),
-    column(
-      width = 9,
-      verticalLayout(
-        # INCLUDE TABLE OF SUMMARY STATISTICS, SOME OF WHICH TO INCLUDE:
-        #   - NUM. LETTERS VS. CORRESPONDING INMATES
-        #   - AVG. NUM. LETTERS PER CORRESPONDING INMATES
-        #   - 
-        # 
-        # DENSITY PLOT BY NUM. LETTERS
-        plotlyOutput("letterDensityPlot"),
-        selectInput(
-          inputId = "timeDimension",
-          label = "Time Dimension",
-          choices = v.time,
-          multiple = FALSE
-        ),
-        plotlyOutput("timeDensityPlot"),
-        selectInput(
-          inputId = "demoDimension",
-          label = "Demographic Dimension",
-          choices = v.char,
-          multiple = FALSE
-        ),
-        plotlyOutput("demoPieChart"),
-        plotlyOutput("demoBarChart")
+    fluidRow(
+      column(
+        width = 6,
+        verticalLayout(
+          # INCLUDE TABLE OF SUMMARY STATISTICS, SOME OF WHICH TO INCLUDE:
+          #   - NUM. LETTERS VS. CORRESPONDING INMATES
+          #   - AVG. NUM. LETTERS PER CORRESPONDING INMATES
+          #   - 
+          # 
+          # DENSITY PLOT BY NUM. LETTERS
+          selectInput(
+            inputId = "demoDimension",
+            label = "Demographic Dimension",
+            choices = v.char,
+            multiple = FALSE
+          ),
+          plotlyOutput("demoPieChart"),
+          plotlyOutput("demoBarChart")
+        )
       )
-    )
-  ),
-  fluidRow()
+    ),
+    fluidRow()
+  )
 )
 
+# application server setup ----
 server <- function(input, output) {
   
   filtered_df <- reactive({
+    origin = "1970-01-01"
+    
     df %>%
       filter(
-        DOB >= as.Date(input$dob[1], origin = "1970-01-01"),
-        DOB <= as.Date(input$dob[2], origin = "1970-01-01"),
-        adminDate >= as.Date(input$admin_date[1], origin = "1970-01-01"),
-        adminDate <= as.Date(input$admin_date[2], origin = "1970-01-01"),
-        ProjectedReleaseDate >= as.Date(input$release[1], origin = "1970-01-01"),
-        ProjectedReleaseDate <= as.Date(input$release[2], origin = "1970-01-01"),
-        `Letter Received` >= as.Date(input$received[1], origin = "1970-01-01"),
-        `Letter Received` <= as.Date(input$received[2], origin = "1970-01-01"),
-        `Letter Processed` >= as.Date(input$processed[1], origin = "1970-01-01"),
-        `Letter Processed` <= as.Date(input$processed[2], origin = "1970-01-01"),
+        # Change POSIX fields to Dates.
+        DOB >= as.Date(input$dob[1], origin = origin),
+        DOB <= as.Date(input$dob[2], origin = origin),
+        adminDate >= as.Date(input$admin_date[1], origin = origin),
+        adminDate <= as.Date(input$admin_date[2], origin = origin),
+        ProjectedReleaseDate >= as.Date(input$release[1], origin = origin),
+        ProjectedReleaseDate <= as.Date(input$release[2], origin = origin),
+        `Letter Received` >= as.Date(input$received[1], origin = origin),
+        `Letter Received` <= as.Date(input$received[2], origin = origin),
+        `Letter Processed` >= as.Date(input$processed[1], origin = origin),
+        `Letter Processed` <= as.Date(input$processed[2], origin = origin),
         {if (is.null(input$mailed_from)) TRUE else Facility_Mailed_From %in% input$mailed_from},
         {if (is.null(input$about)) TRUE else Facility_About %in% input$about},
         {if (is.null(input$code)) TRUE else Code %in% input$code},
@@ -200,37 +217,32 @@ server <- function(input, output) {
       )
   })
   
-  output$letterDensityPlot <- renderPlotly({
-    df <-
-      filtered_df() %>%
-      count(DOC_Number)
-    
-    plot_ly(
-      x = ~df$n,
-      type = 'histogram',
-      xbins = list('size' = 1)
-    ) %>%
-      layout(
-        title = "Number of Corresponding Inmates by Letters Sent in Given Dataset",
-        xaxis = list(title = "Number of Letters Sent per Inmate"),
-        yaxis = list(title = "Number of Corresponding Inmates")
-      )
+  output$letterTable <- renderDT({
+    filtered_df() %>%
+      count(DOC_Number, name = "Number of Letters per Correspondent") %>%
+      count(`Number of Letters per Correspondent`, name = "Number of Correspondents") %>%
+      add_row(
+        `Number of Letters per Correspondent` = "Total Correspondents",
+        `Number of Correspondents` = sum(.$`Number of Correspondents`),
+        .before = 1
+      ) %>%
+      datatable(rownames = FALSE)
   })
   
   output$timeDensityPlot <- renderPlotly({
     df <- filtered_df()
-    
+
     # Selecting <<distinct>> inmates when determining demographic data
     if (input$timeDimension %in% c('DOB', 'adminDate', 'ProjectedReleaseDate')) {
       df <-
         df %>%
         distinct(DOC_Number, .keep_all = TRUE)
-      
+
       title_unit = "Corresponding Inmates"
     } else {
       title_unit = "Letters"
     }
-      
+
     plot_ly(
       x = ~df[[input$timeDimension]],
       type = 'histogram' #,
